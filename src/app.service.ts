4661a2c35baa184e58f95fca7b8352ba3dfdf03d
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2024-11-23 11:39:38 -0300

    feature: add conection db postgres

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaServicePostgres } from './infra/port/out/database/prisma-postgres.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private postgresService: PrismaServicePostgres){}
+
+  async getHello(): Promise<string> {
+    console.log('Tentando criar um novo estabelecimento...');
+    try {
+      await this.postgresService.establishment.create({
+        data: {
+          name: "Davi",
+          description: "Teste",
+          location: "Santo Andre",
+        },
+      });
+      console.log('Estabelecimento criado com sucesso.');
+    } catch (error) {
+      console.error('Error creating establishment:', error);
+    }
     return 'Hello World!';
   }
 }
